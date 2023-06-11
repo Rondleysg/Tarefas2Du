a6ce7c2b5938782b9be7c831e4c886cf3954b7ae
Rondley Gregório <80141884+Rondleysg@users.noreply.github.com>
2023-06-11 11:53:50 -0300

    fix: sort tasks in myday page

--- src/pages/MyDay/index.tsx
+++ src/pages/MyDay/index.tsx
@@ -72,6 +72,9 @@ export default function MyDay({navigation}: MyDayProps) {
         <FlatList
           data={tasksToday.sort((a, b) => {
             if (a.completeDate && b.completeDate) {
+              if (a.completeDate === b.completeDate) {
+                return 0;
+              }
               return a.completeDate < b.completeDate ? -1 : 1;
             }
             return 0;
